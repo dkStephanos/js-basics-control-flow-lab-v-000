97e7f390249f14b0c244bebdc70d5b2c08a65ab1
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-03 20:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
-    "This one is on me!"
+    return "This one is on me!"
   } else if (feet > 2000) {
     "I will gladly take your thirty bucks."
   } else if (feet > 2500) {
